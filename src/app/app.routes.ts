f2bdec8d8023835d101b5ee282b629e5d5f7d221
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-04-24 18:32:16 +0200

    Skapat routing till de olika undersidorna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Convert } from './convert/convert';
+import { About } from './about/about';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "start", component: Home },
+    { path: "konvertera", component: Convert },
+    { path: "om", component: About },
+
+    //Route för tom sökväg
+    { path: "", redirectTo: "start", pathMatch: "full" },
+
+    //Route till 404-sida
+    { path: "404", component: NotFound },
+    { path: "**" , redirectTo: "404", pathMatch: "full" }
+
+
+];
