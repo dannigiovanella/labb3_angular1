8d2bdebfb9709e278064fd5a146aaa2125f0e910
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-04-25 17:57:41 +0200

    Skapat metoder för att konvertera längd och temperatur

--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -8,5 +8,41 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './convert.css',
 })
 export class Convert {
-  
+  // Längd
+  // Lagrar längdvärden från inputfälten
+  meter: number = 0;
+  foot: number = 0;
+
+
+  // Temperatur
+  // Lagrar temperaturvärden från inputfälten
+  celcius: number = 0;
+  fahrenheit: number = 0;
+
+
+  //Metoder
+
+  //Konverterar längd
+
+  //Meter till foot
+  convertMeterToFoot(): void {
+    this.foot = this.meter * 3.28084;
+  }
+
+  //Foot till meter
+  convertFootToMeter(): void {
+    this.meter = this.foot / 3.28084;
+  }
+
+  //Konverterar temperatur
+   convertCelToFar(): void {
+    this.fahrenheit = (this.celcius * 9 / 5) + 32;
+  }
+
+    convertFarToCel(): void {
+    this.celcius = (this.fahrenheit - 32) * 5 / 9;
+  }
+
 }
+
+
